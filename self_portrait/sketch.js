389b5f69b10ef008993649266de3fe0c6df1a868
jon darling <jondarling@jons-MacBook-Pro-2.local>
2019-01-30 00:15:08 -0500

    characters code added

--- self_portrait/sketch.js
+++ self_portrait/sketch.js
@@ -1,104 +1,34 @@
 /*
-self portrait
+wk1_310
 v1.
 by Jon Darling
-Hard coded V_1
+characters
 */
 
 function setup() {
-	createCanvas(640, 450);
+	createCanvas(500, 500);
+   
 }
 
 function draw() {
-    // Face
-	background("#F7F1DA");
-    
-	noStroke();
-     
-    // Facial lines
-    stroke (5)
-    
-	line(40, 120, 40, 20); // two xy coordinates
-    line(70, 300, 40, 120); // two xy coordinates
-    line(130, 400, 70, 300); // two xy coordinates
-     line(130, 400, 280,500); // two xy coordinates
-	// after 2 points a fill is added
-     
-	line(600, 120, 600, 20); // two xy coordinates
-    line(570, 300, 600, 120); // two xy coordinates
-    line(510, 400, 570, 300); // two xy coordinates
-     line(510, 400, 330,500); // two xy coordinates
-    
-    
+   
    
     
-   
-     // nose
-	fill("#917A4F");
-    noStroke()
-//    ellipse(317, 200,80, 120);
-    	triangle(300, 100, 300, 50,330, 220); // three xy coordinates
-    // nostrils
-    fill ("black")
-     ellipse(300, 200,20, 10);
-	ellipse(440, 94, 20, 35);
-    	
-   
-    noStroke()
-     arc(315, 300, 250, 200, 0, PI);
-    // mouth
-    stroke("black")
-    strokeWeight(5)
-     fill("red")
-      arc(300, 300, 250, 200, 0, PI);
-    // teeth
-    fill ("white")
-    arc(330,320, 200, 100, 0, PI);
-   
-      
-     // bags
-    noStroke()
-    fill (210)
-     triangle(270,80, 70,180, 220, 110); // three xy coordinates
-     triangle(550, 150,400,100, 500, 150); // three xy coordinates
+     background(233);
     
-      // whites eyes
-    fill ("white")
-     ellipse (450, 80, 130, 85);
-    ellipse (170, 80, 130, 85);
-     
-    
-    
-  
-    
-    // eyes
-    noStroke();
     
     fill ("blue")
-    ellipse(450, 80, 80);// right
-    ellipse(175, 80, 80);// left
-     // eyebrows
-    noStroke();
-    fill ("#917A4F")
+    noStroke()
+	ellipse(250, 300, 100, 100);
+    	
+    // arc
     
-    	 ellipse (450, 18, 130, 20);
-    ellipse (170, 20, 130, 20);
-   
-   
-   
-    
-    fill ("black")
-     			
-    
-    // pupils
-    ellipse(180, 93, 20, 35);
-	ellipse(440, 94, 20, 35);
-    
+    stroke("black")
+    strokeWeight(2)
+    fill("green")
+    arc(250, 100, 250, 200, 0, PI);
+
     
    
 }
 
-
-     	
-    
-   
