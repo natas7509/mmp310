661ea2318dbf912b7b57ff5a89640451ac741a79
jon darling <jondarling@jons-MacBook-Pro-2.local>
2019-02-03 15:07:27 -0500

    Characters completion

--- Characters/sketch.js
+++ Characters/sketch.js
@@ -67,20 +67,33 @@ var blueY = 300;
 var blueSize = 200;
 var blueEyeSize = blueSize / 8;
 
-var bananaX = 600;
-var bananaY = 100;
-var bananaSize = 100;
+var dartX1 = 655;
+var dartY1 = 531;
+var dartX2 = 600;
+var dartY2 = 420;
+var dartX3 = 590;
+var dartY3 = 463;
+var dartX4 = 555;
+var dartY4 = 476;
 
-var story = "welcome to the blueman group.";
+var mouthX = 640;
+var mouthY = 330;
+var mouthW = 50;
+var mouthH = 50;
+
+
+
+var story = "The adventures of Big Head.";
 var storyX;
 var storyY;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	textSize(40);
+	textSize(38);
+    textFont('roboto');
 	textAlign(CENTER, CENTER);
 	storyX = width / 2;
-	storyY = height - 50;
+	storyY = height - 60;
 }
 
 function draw() {
@@ -89,24 +102,24 @@ function draw() {
 	// first character blueHead
 	fill('blue');
 	noStroke();
-	ellipse(blueX, blueY, blueSize);  	// body
+	ellipse(blueX, blueY, blueSize);  	// head
     
 	fill ('white');
 	noStroke();
-	ellipse(blueX + blueEyeSize * 3, blueY - blueEyeSize, blueEyeSize);  // left eye
-	ellipse(blueX, blueY - blueEyeSize, blueEyeSize); 		// right eye
+	ellipse(blueX + blueEyeSize * 2.75, blueY - blueEyeSize, blueEyeSize);  // left eye
+	ellipse(blueX - blueEyeSize *.5, blueY - blueEyeSize, blueEyeSize,); // right eye
+    arc(mouthX, mouthY, mouthW, mouthH, 0, PI + QUARTER_PI,OPEN); //mouth
 	
-//	// second character banana
-//	fill('yellow');
-//	noStroke();
-//	arc(bananaX, bananaY, bananaSize, bananaSize, -HALF_PI, HALF_PI); // banana background
-//	fill(50);
-//	arc(bananaX, bananaY, bananaSize / 2, bananaSize, -HALF_PI, HALF_PI); // banana foreground
-//	noFill();
-//	stroke(0);
-//	arc(bananaX, bananaY, bananaSize * 3/4, bananaSize, -HALF_PI, HALF_PI); // banana contour
-	
-	stroke(255);
+	// second character dart body
+	fill('red');
+	noStroke();
+    quad(dartX1, dartY1, dartX2, dartY2, dartX3, dartY3, dartX4, dartY4,);
+   
+    //text
+    textStyle('italic');
+    fill('white');
+	stroke('rgb(0,255,0)');
+    strokeWeight(1.7);
 	text(story, storyX, storyY);
 }
 
