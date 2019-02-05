1e94b4c987f37b86e1cfdd7f3fc6eb3a907ca2e5
natas7509#3 <sirjondarling@gmail.com>
2019-02-04 19:15:39 -0500

    update characters inclass

--- Characters/sketch.js
+++ Characters/sketch.js
@@ -6,60 +6,7 @@ characters sketch
 Interactive Story
 */
 
-//function setup() {
-//	createCanvas(windowWidth, windowHeight);
-//    textSize(40);    
-//}
-//
-////global variables
-//var blueX = 200;
-//var blueY = 300;
-//var blueSize = 200;
-//var blueEyeSize = 100;
-//
-//var story
-//
-//
-//function draw() {
-//   
-//   
-//    
-//     background(233);
-//    
-//    
-//    // blueman character 
-//   
-//    fill ("blue");
-//    stroke(0);
-//	ellipse(blueX + blueEyeSize * 3, blueY - blueEyeSize, blueEyeSize);  // left eye
-//	ellipse(blueX, blueY - blueEyeSize, blueEyeSize); 		// right eye
-//	
-//    
-//    
-//    //eyes
-//    fill ('white');
-//    ellipse(220, 300, 20);
-//    ellipse(180, 300, 20);
-//    	
-//    // 2nd character
-//    
-//    stroke("black")
-//    strokeWeight(2)
-//   
-//    
-//    
-////    text("checking text function." width/2, height - 50);
-//    
-//    // owen example
-//    
-//    fill ("yellow");
-//    arc(600, 300, 200, 200, 0, PI);
-//    fill (233);
-//  
-//
-//    
-//   
-//}
+
 
 // global variables
 var blueX = 600;
@@ -67,60 +14,98 @@ var blueY = 300;
 var blueSize = 200;
 var blueEyeSize = blueSize / 8;
 
-var dartX1 = 655;
-var dartY1 = 531;
-var dartX2 = 600;
-var dartY2 = 420;
-var dartX3 = 590;
-var dartY3 = 463;
-var dartX4 = 555;
-var dartY4 = 476;
 
 var mouthX = 640;
 var mouthY = 330;
 var mouthW = 50;
 var mouthH = 50;
 
+// red character
+
+var redX = 1000;
+var redY = 300;
+var redSize = 150;
+var redEyeSize = redSize / 8;
 
 
-var story = "The adventures of Big Head.";
+var redmouthX = 1000;
+var redmouthY = 330;
+var redmouthW = 30;
+var redmouthH = 40;
+
+
+
+var story = "The adventures of the Popsicles.";
 var storyX;
 var storyY;
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	textSize(38);
+    createCanvas(windowWidth, windowHeight);
+    textSize(38);
     textFont('roboto');
-	textAlign(CENTER, CENTER);
-	storyX = width / 2;
-	storyY = height - 60;
+    textAlign(CENTER, CENTER);
+    storyX = width / 2;
+    storyY = height - 60;
 }
 
 function draw() {
-	background(50);
-	
-	// first character blueHead
-	fill('blue');
-	noStroke();
-	ellipse(blueX, blueY, blueSize);  	// head
+    background(50);
+
+    // first character 
+    fill('blue');
+    noStroke();
+    ellipse(blueX, blueY, blueSize); // head
+
+    fill('white');
+    noStroke();
+    ellipse(blueX + blueEyeSize * 2.75, blueY - blueEyeSize, blueEyeSize); // left eye
+    ellipse(blueX - blueEyeSize * .5, blueY - blueEyeSize, blueEyeSize, ); // right eye
+    arc(mouthX, mouthY, mouthW, mouthH, 0, PI + QUARTER_PI, OPEN); //mouth
+
+    //body
+    fill('blue');
+    rect(575, 400, 60, 200, 100);
+    fill('white');
+    rect(500, 400, 200, 100, 20);
     
-	fill ('white');
-	noStroke();
-	ellipse(blueX + blueEyeSize * 2.75, blueY - blueEyeSize, blueEyeSize);  // left eye
-	ellipse(blueX - blueEyeSize *.5, blueY - blueEyeSize, blueEyeSize,); // right eye
-    arc(mouthX, mouthY, mouthW, mouthH, 0, PI + QUARTER_PI,OPEN); //mouth
-	
-	// second character dart body
-	fill('red');
-	noStroke();
-    quad(dartX1, dartY1, dartX2, dartY2, dartX3, dartY3, dartX4, dartY4,);
-   
+     //Neck 
+    noStroke();
+    fill(190); // neck color   
+    arc(600, 400, 200, 20, 100, PI + QUARTER_PI, ); // Neck shape  
+        
+         // 2nd character 
+    fill('red');
+    noStroke();
+    ellipse(redX, redY, redSize); // head
+
+    fill('white');
+    noStroke();
+    ellipse(redX + redEyeSize * 2.75, redY - redEyeSize, redEyeSize); // left eye
+    ellipse(redX - redEyeSize * .5, redY - redEyeSize, redEyeSize, ); // right eye
+    arc(redmouthX, redmouthY, redmouthW, redmouthH, 0, PI + QUARTER_PI, OPEN); //mouth
+
+    //body
+    fill('red');
+    rect(970, 400, 60, 200, 100);
+    fill('white');
+    rect(900, 380, 200, 100, 20);
+    
+     //Neck 
+    noStroke();
+    fill(190); // neck color   
+    arc(1000, 375, 170, 20, 100, PI + QUARTER_PI, ); // Neck shape  
+
     //text
     textStyle('italic');
     fill('white');
-	stroke('rgb(0,255,0)');
+    stroke('rgb(0,255,0)');
     strokeWeight(1.7);
-	text(story, storyX, storyY);
+    text(story, storyX, storyY);
+
+
+
+
+
 }
 
 
