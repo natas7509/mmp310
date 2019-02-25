4ff13b222c62dcc78001dd6c000e80fd26ebba67
jon darling <jondarling@jons-MacBook-Pro-2.local>
2019-02-25 00:08:58 -0500

    loops-home-update

--- loops/sketch.js
+++ loops/sketch.js
@@ -6,31 +6,31 @@ characters sketch
 loops
 */
 
+var s = 200;
+var y = 400;
+var eyeSize = 20;
+var mouth
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    background('grey');
-
-
+    background(30,100,16);
     rectMode(CENTER);
-
-
     noStroke();
 
-    for (let x = 100; x < width; x += 400) {
-//        fill('yellow');
-        ellipse(x, height/2, 200); // face
-     
-        var c = map (x, 0, 4, 0, 255);
-        fill(c);
-        ellipse(x + 50,height/2-35, 50, 50); // left eye
-        ellipse(x - 50,height/2-35, 50, 50); // right eye
+    for (let x = 100; x < width; x += 200) {
+        fill('plum');
+        ellipse(x, y, s); // face
+        fill(0);
+        ellipse(x + eyeSize * 2, y - 20, 50, 20); // left ey2
+        ellipse(x - eyeSize * 2, y - 20, 50, 20); // right eye
         fill('gold');
-        rect(x, height/2+50, 66, 25, 22); // mouth
+        rect(x, y + 60, 80, 30, 20); // mouth
         fill('blue');
-        rect(x, height/2, 25, 40, 22); // nose
+        rect(x, y + 20, 20, 40, 10); //nose
     }
+   
 
-    
+    	
+
 
 }
