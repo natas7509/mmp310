17fcdca6cfca8714255f98c97e7e5fd3b23f1b6c
jon darling <jondarling@jons-mbp-2.bmcc.cuny.edu>
2019-03-06 13:43:17 -0500

    added grid lines

--- patterns/sketch.js
+++ patterns/sketch.js
@@ -7,84 +7,91 @@ grids/patterns
 */
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background("#F5D3C8");
+    createCanvas(windowWidth, windowHeight);
+    background("#F5D3C8");
 
-  var columns = 12;
-  var rows = 6;
-  var w = width / columns; // column width
-  var h = height / rows; // row height
-  var wingLength = random(0, 20);
+    var columns = 12;
+    var rows = 6;
+    var w = width / columns; // column width
+    var h = height / rows; // row height
+    var wingLength = random(0, 20);
 
-  for (let x = 0; x <= width; x += w) {
-    for (let y = 0; y <= height; y += h) {
-      ellipse(x, y, w / 15);
-      noFill();
-      noStroke();
+    for (let x = 0; x <= width; x += w) {
+        for (let y = 0; y <= height; y += h) {
 
-      var randomChoice = random(2);
-      if (randomChoice > 1) {
-        var r = random(0, x);
-        var g = random(0, y);
-        var b = random(100, 200);
-        fill(r, g, b);
-        ellipse(x, y, w / 2, 15); //bird body one - ellipse
-          
-          
-          //wings - ellipse-set
-          
-        noStroke();
-        fill("blue");
-        triangle(
-          x,
-          y,
-          x + 58 + wingLength,
-          y + 20 + wingLength,
-          x + 86 + wingLength,
-          y + 75 + wingLength
-        );
-        fill("#5B4B49");
-        triangle(
-          x,
-          y,
-          x - 58 + wingLength,
-          y - 20 + wingLength,
-          x - 86 + wingLength,
-          y - 75 + wingLength
-        );
-      } else {
-        stroke("black");
-        fill("#4A7B9D");
-        noStroke();
 
-        var r = random(0, x);
-        var g = random(0, y);
-        var b = random(255);
 
-        fill(r, g, b);
-        rect(x - 20, y - 20, w / 4, h / 3, 20); //bird body two - rect
+            stroke("#4A7B9D");
+            strokeWeight(1);
+            line(x / 2 - width / 2, y, x, y);//vert-grid
+            line(x, y / 2 - height / 2, x, y);//hor-grid
+            ellipse(x, y, w / 15);
+            noFill();
+            noStroke();
 
-          //wings - rect-set
-          
-        fill("#C0D6DF");
-        triangle(
-          x,
-          y,
-          x - 58 + wingLength,
-          y - 20 + wingLength,
-          x - 86 + wingLength,
-          y - 75 + wingLength
-        );
-        fill("#9AA899");
-        triangle(
-          x,
-          y,
-          x + 58 + wingLength,
-          y + 20 + wingLength,
-          x + 86 + wingLength,
-          y + 75 + wingLength
-        );
-      }
+            var randomChoice = random(2);
+            if (randomChoice > 1) {
+                var r = random(0, x);
+                var g = random(0, y);
+                var b = random(100, 200);
+                fill(r, g, b);
+                ellipse(x, y, w / 2, 15); //bird body one - ellipse
+
+                //wings - ellipse-set
+
+                noStroke();
+                fill("blue");
+                triangle(
+                    x,
+                    y,
+                    x + 58 + wingLength,
+                    y + 20 + wingLength,
+                    x + 86 + wingLength,
+                    y + 75 + wingLength
+                );
+                fill("#5B4B49");
+                triangle(
+                    x,
+                    y,
+                    x - 58 + wingLength,
+                    y - 20 + wingLength,
+                    x - 86 + wingLength,
+                    y - 75 + wingLength
+                );
+            } else {
+                stroke("black");
+                fill("#4A7B9D");
+                noStroke();
+
+                var r = random(0, x);
+                var g = random(0, y);
+                var b = random(255);
+
+                fill(r, g, b);
+                rect(x - 14, y - 20, w / 4, h / 3, 20); //bird body two 
+              
+
+                //wings - rect-set
+
+                fill("#C0D6DF");
+                triangle(
+                    x,
+                    y,
+                    x - 58 + wingLength,
+                    y - 20 + wingLength,
+                    x - 86 + wingLength,
+                    y - 75 + wingLength
+                );
+                fill("#9AA899");
+                triangle(
+                    x,
+                    y,
+                    x + 58 + wingLength,
+                    y + 20 + wingLength,
+                    x + 86 + wingLength,
+                    y + 75 + wingLength
+                );
+            }
+        }
     }
-  }
 }
