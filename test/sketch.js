17076eb60ab892713b2ddd478af378f5178bf211
jon darling <jondarling@jons-MacBook-Pro-2.local>
2019-03-09 01:09:44 -0500

    added images*3

--- test/sketch.js
+++ test/sketch.js
@@ -5,65 +5,60 @@ grid/image
 */
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    background(220);
+  createCanvas(windowWidth, windowHeight);
+  background(220);
+  rectMode(CENTER);
+  textAlign(CENTER, CENTER);
 
-    var columns = 8;
-    var rows = 6;
-    var w = width / columns;
-    var h = height / rows;
+  var columns = 9;
+  var rows = 7;
+  var w = width / columns;
+  var h = height / rows;
+  //    var cap = 'hello';
 
-    for (let x = 0; x <= width; x += w) {
-        for (let y = 0; y <= height; y += h) {
+  for (let x = 0; x <= width; x += w) {
+    for (let y = 0; y <= height; y += h) {
+      discs(x - w, y - h, w, h); //discs
 
+      var rd = random(3);
+      if (rd > 2) {
+        stroke(0);
+        strokeWeight(1.6);
+        line(x + w, y, x + w, y + h);
+      } else if (rd > 1) {
+        var r = random(0, 255);
+        var g = random(0, 100);
+        var b = random(0, 255);
 
-         
-            var r = random(0, 255);
-            var g = random(0, 100);
-            var b = random(0, 255);
+        stroke(r, g, b);
+        strokeWeight(2);
+        line(x + w, y, x, y + h);
+      } else {
+        stroke(0);
+        strokeWeight(1);
+        line(x, y, x + w, y + h);
+        discs(x - w, y - h, w, h); //discs
+        noStroke();
 
-            fill(r, g, b);
-            noStroke();
-            ellipse(x, y, w / 12);
-
-
-            var rd = random(3);
-            if (rd > 2) {
-                stroke(0);
-                strokeWeight(1.6); //verticle lines
-                line(x + w, y - h, x + w, y + h);
-
-
-
-            } else if (rd > 1) {
-
-                stroke(0);
-                strokeWeight(2);
-                line(x - w, y + h, x + w, y - h);
-                fill(r, g, b);
-
-
-
-
-
-            } else {
-
-                stroke(0);
-                strokeWeight(3); 
-                line(x + w, y + h, x - w, y - h);//angle left to right
-                stroke(0);
-                strokeWeight(2);
-                line(x + w, y - h, x + w, y + h);
-                
-       
-
-
-            }
-        }
+        rect(x - w, y - h, w / 4, h / 4, 10); // blocks
+      }
     }
+  }
+}
+
+function discs(x, y, w, h) {
+  var r = random(0, 255);
+  var g = random(0, 100);
+  var b = random(0, 255);
+  fill(r, g, b);
+  noStroke();
+  ellipse(x, y, w / 6);//main disc
+  stroke(255);
+  strokeWeight(2);
+  ellipse(x, y, 7, 3 + random(5));//inner disc
 }
 
 // click to save image to Downloads folder
-//function mouseClicked() {
-//	save('pattern-1.jpg');
-//}
+function mouseClicked() {
+	save('pattern-1.jpg');
+}
