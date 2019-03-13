675cdce0064d8bb91405c6449c0f8f2ca3476282
jon darling <jondarling@jons-mbp-2.bmcc.cuny.edu>
2019-03-13 16:36:46 -0400

    added button for interface

--- Interface/sketch.js
+++ Interface/sketch.js
@@ -4,45 +4,113 @@ MMP-310
 interface
 */
 
-var bg = "green";
 
 function setup() {
   createCanvas(windowWidth, windowHeight / 2);
-
-  button.mousePressed("save Image");
-  button.style("font-family", "roboto");
-  button.id = "my-button";
-  button.class("interface");
-  var button = createButton("Save Image");
-  var generatePattern = createButton("generate Pattern");
-  generatePattern.mousePressed.mousePressed(pattern);
-
   background(220);
   rectMode(CENTER);
   textAlign(CENTER, CENTER);
 
-  var columns = 8;
-  var rows = 8;
+  var columns = 9;
+  var rows = 7;
   var w = width / columns;
   var h = height / rows;
+  //    var cap = 'hello';
+  var button = createButton("Save Image");
+    var button = createButton("generate pattern");
+    button.mousePressed("generate pattern");
+   
 
-  // nested for loop
   for (let x = 0; x <= width; x += w) {
     for (let y = 0; y <= height; y += h) {
-      // line(x1, y1, x2, y2);
-      stroke(255);
-      strokeWeight(2);
-      var r = random(3);
-      if (r > 2) {
+      discs(x - w, y - h, w, h); //discs
+
+      var rd = random(3);
+      if (rd > 2) {
+        stroke(0);
+        strokeWeight(1.6);
         line(x + w, y, x + w, y + h);
-      } else if (r > 1) {
-        line(x, y, x + w, y + h);
-      } else {
+      } else if (rd > 1) {
+        var r = random(0, 255);
+        var g = random(0, 100);
+        var b = random(0, 255);
+
+        stroke(r, g, b);
+        strokeWeight(2);
         line(x + w, y, x, y + h);
+      } else {
+        stroke(0);
+        strokeWeight(1);
+        line(x, y, x + w, y + h);
+        discs(x - w, y - h, w, h); //discs
+        noStroke();
+
+        rect(x - w, y - h, w / 4, h / 4, 10); // blocks
       }
     }
   }
 }
+
+function discs(x, y, w, h) {
+  var r = random(0, 255);
+  var g = random(0, 100);
+  var b = random(0, 255);
+  fill(r, g, b);
+  noStroke();
+  ellipse(x, y, w / 6);//main disc
+  stroke(255);
+  strokeWeight(2);
+  ellipse(x, y, 7, 3 + random(5));//inner disc
+}
+
+
+// click to save image to Downloads folder
+//function mouseClicked() {
+//	save('pattern-1.jpg');
+//}
+
+
+
+//var bg = "green";
+//
+//function setup() {
+//  createCanvas(windowWidth, windowHeight / 2);
+//    pattern();
+//
+////  button.mousePressed("generate pattern");
+////  button.style("font-family", "roboto");
+////  button.id = "my-button";
+////  button.class("interface");
+//  var button = createButton("Save Image");
+//  var generatePattern = createButton("generate Pattern");
+//  generatePattern.mousePressed.mousePressed(pattern);
+//
+//  background(220);
+//  rectMode(CENTER);
+//  textAlign(CENTER, CENTER);
+//
+//  var columns = 8;
+//  var rows = 8;
+//  var w = width / columns;
+//  var h = height / rows;
+//
+//  // nested for loop
+//  for (let x = 0; x <= width; x += w) {
+//    for (let y = 0; y <= height; y += h) {
+//      // line(x1, y1, x2, y2);
+//      stroke(255);
+//      strokeWeight(2);
+//      var r = random(3);
+//      if (r > 2) {
+//        line(x + w, y, x + w, y + h);
+//      } else if (r > 1) {
+//        line(x, y, x + w, y + h);
+//      } else {
+//        line(x + w, y, x, y + h);
+//      }
+//    }
+//  }
+//}
 
 //    for (let x = 0; x <= width; x += w) {
 //        for (let y + 0; y <= height; y += h) {
