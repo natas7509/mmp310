094875769f9caaca0753743dc4a8eaeb1dc36712
jon darling <jondarling@jons-MacBook-Pro-2.local>
2019-03-31 21:59:25 -0400

    dataVis final

--- CSV/sketch.js
+++ CSV/sketch.js
@@ -1,11 +1,7 @@
-/*
-	jon darling
-    MMP-310
-    wekk-8
-    DATA v-1
-    
- */
-
+/* 
+	nba rookies data vis
+	mmp 310 week 8
+*/
 
 var rookies;
 function preload() {
@@ -16,34 +12,50 @@ function setup() {
 	createCanvas(windowWidth, windowHeight);
 	textSize(50);
 	textAlign(LEFT, CENTER);
-    rectMode(CENTER);
 	noStroke();
 }
 
 function draw() {
-	background('#888');
+	background(51);
 	
 	for (let i = 0; i < rookies.getRowCount(); i++) {
 		let ast = rookies.getNum(i, 'ASTper');
 		let min = rookies.getNum(i, 'MPper');
-		let x = map(min, 0, 39, 0, width);
-		let y = map(ast, 0, 8, height - 10, 0);
+		let x = i*9;
+		let y = map(ast, 0, 8, height, 0);
 		
 		fill('white');
-		if (dist(x, y, mouseX, mouseY) < 7.5) {
+		if (dist(x, y, mouseX, mouseY) < 5) {
 			let player = rookies.getString(i, 'Player').split('\\')[0];
-            fill('blue');
 			text(player, 100, 200);
-            fill('red');
 			text(ast, 100, 250);
-            fill('white');
 			text(min, 100, 300);
-				
+			fill('gold');	
 		}
-        
-		rect(x, y, 15, 15);
+		rect(x, y, 8, height + x);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
