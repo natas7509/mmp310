fb8cf89c8e0ae0e2b5327f6d84f350955a11d0e0
jon darling <jondarling@jons-MacBook-Pro.local>
2019-05-04 19:04:34 -0400

    reset Entity

--- Final/entity.js
+++ Final/entity.js
@@ -2,12 +2,48 @@
 	entity super class
 */
 
-class Entity {
+//class Entity {
+//	constructor(x, y) {
+//		this.x = x;
+//		this.y = y;
+//		this.size = width * 0.07;
+//  		this.speed = { x: 0, y: 0 };
+//	}
+//	
+//	update() {
+//		this.x += this.speed.x;
+//		this.y += this.speed.y;
+//	}
+//	
+//	collide(other) {
+//		var d = dist(this.x, this.y, other.x, other.y);
+//		var size;
+//		if (this.size > other.size) {
+//			size = this.size;
+//		} else {
+//			size = other.size / 2;
+//		}
+//		if (d < size * 1.5) {
+//			return true;
+//		} else {
+//			return false;
+//		}
+//	}
+//	
+//	remove(list) {
+//		var index = list.indexOf(this);
+//		list.splice(index, 1);
+//	}
+//    
+//    
+//}
+
+ class Entity {
 	constructor(x, y) {
 		this.x = x;
 		this.y = y;
-		this.size = width * 0.07;
-  		this.speed = { x: 0, y: 0 };
+		this.size = width * 0.15;
+		this.speed = { x: 0, y: 0 };
 	}
 	
 	update() {
@@ -21,9 +57,9 @@ class Entity {
 		if (this.size > other.size) {
 			size = this.size;
 		} else {
-			size = other.size / 2;
+			size = other.size;
 		}
-		if (d < size * 1.5) {
+		if (d < size) {
 			return true;
 		} else {
 			return false;
@@ -34,11 +70,8 @@ class Entity {
 		var index = list.indexOf(this);
 		list.splice(index, 1);
 	}
-    
-    
 }
 
- 
 
 
 
