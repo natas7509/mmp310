d6bc558f82a1d98aeb99938e7048feeb9504b08b
jon darling <jondarling@jons-MacBook-Pro.local>
2019-05-04 20:26:07 -0400

    testing setSpeed

--- Final/entity.js
+++ Final/entity.js
@@ -30,23 +30,7 @@ class Entity {
 		}
 	}
     
-//    	collide(other) {
-//		var d = dist(this.x, this.y, other.x, other.y);
-//		var size;
-//		if (this.size > other.size) {
-//			size = this.size;
-//		} else {
-//			size = other.size;
-//		}
-//		if (d < size) {
-//			return true;
-//		} else {
-//			return false;
-//		}
-//	}
-    
-  
-	
+
 	remove(list) {
 		var index = list.indexOf(this);
 		list.splice(index, 1);
@@ -54,50 +38,6 @@ class Entity {
     
     
 }
-
-// class Entity {
-//	constructor(x, y) {
-//		this.x = x;
-//		this.y = y;
-//		this.size = width * 0.04;
-//		this.speed = { x: 0, y: 0 };
-//	}
-//	
-//	update() {
-//		this.x += this.speed.x;
-//		this.y += this.speed.y;
-//	}
-//	
-//	collide(other) {
-//		var d = dist(this.x, this.y, other.x, other.y);
-//		var size;
-//		if (this.size > other.size) {
-//			size = this.size;
-//		} else {
-//			size = other.size;
-//		}
-//		if (d < size) {
-//			return true;
-//		} else {
-//			return false;
-//		}
-//	}
-//	
-//	remove(list) {
-//		var index = list.indexOf(this);
-//		list.splice(index, 1);
-//	}
-//}
-
-
-
-
-
-
-
-
-
-
 
 
 
